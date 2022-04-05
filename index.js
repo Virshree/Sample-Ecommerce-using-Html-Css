0942ffe04f9fe3e132f075243101ec9ab4f0efe2
Viru-190197 <virshreedesai191@gmail.com>
2022-04-05 14:02:23 +0530

    Files added

---
+++ index.js
@@ -0,0 +1,21 @@
+// arr =[1,"hi",true,"mikhil"];
+// console.log(arr);
+// console.log(arr.length)
+
+// var s="mikhil";
+// console.log(s.toUpperCase())
+
+// var s="10 + 10";
+// var sobj =new String("10 +10");
+// console.log(eval(s));
+// console.log(eval(sobj));
+
+function equalto()
+{
+    var num=10;
+    if(num==="10")
+     return true;
+    else
+     return false;
+}
+console.log(equalto());
---
+++ ladies.js
@@ -0,0 +1,5 @@
+function searchFilter() {
+  let filter = document.getElementById("search").value.toUpperCase();
+  let div = document.getElementsById("myTop");
+  let topnames = document.getElementsByTagName("top-names");
+}
